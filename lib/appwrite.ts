f54b731f95b42142875c0866f30edf5fb9ff3628
Cool Mugwagwa <c]m>
2026-09-22 14:22:16 +0200

    Fix Vercel build crash when Appwrite env vars are missing

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -31,10 +31,13 @@ export const config = {
 };
 
 export const client = new Client();
-client
-  .setEndpoint(config.endpoint!)
-  .setProject(config.projectId!)
-  .setPlatform(config.platform!);
+
+if (config.endpoint && config.projectId) {
+  client
+    .setEndpoint(config.endpoint)
+    .setProject(config.projectId)
+    .setPlatform(config.platform!);
+}
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
